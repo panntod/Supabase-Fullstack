6fb8308e8532e308e865c72d9fa3ef14c931e5fc
panntod <pandhu.munjalindra@gmail.com>
2024-05-13 08:21:31 +0700

    feat: Create index actions and connection supabase

---
+++ src/lib/actions/index.ts
@@ -0,0 +1,8 @@
+"use server";
+
+import createSupabaseServerClient from "../supabase/server";
+
+export default async function  readUserSession(){
+    const supabase = await createSupabaseServerClient()
+    return supabase.auth.getSession()
+}
