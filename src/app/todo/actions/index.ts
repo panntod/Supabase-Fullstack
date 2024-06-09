eebe6b8e9ccb74aaa1f43151ce35f9c13643930f
panntod <pandhu.munjalindra@gmail.com>
2024-06-10 05:23:29 +0700

    feat: Create function create, read, update and delete

--- src/app/todo/actions/index.ts
+++ src/app/todo/actions/index.ts
@@ -1,7 +1,30 @@
-export async function createTodo(title: string) {}
+"use server";
 
-export async function readTodo() {}
+import createSupabaseServerClient from "@/lib/supabase/server";
+import { revalidatePath, unstable_noStore as noStore } from "next/cache";
 
-export async function deleteTodoById(id: string) {}
+export async function createTodo(title: string) {
+  const supabase = await createSupabaseServerClient();
+  const result = await supabase.from("todos").insert({ title }).single();
 
-export async function updateTodoById(id: string, completed: boolean) {}
+  revalidatePath("/todo");
+  return JSON.stringify(result);
+}
+
+export async function readTodo() {
+  noStore();
+  const supabase = await createSupabaseServerClient();
+  return await supabase.from("todos").select("*");
+}
+
+export async function deleteTodoById(id: string) {
+  const supabase = await createSupabaseServerClient();
+  await supabase.from("todos").delete().eq("id", id);
+  revalidatePath("/todo");
+}
+
+export async function updateTodoById(id: string, completed: boolean) {
+  const supabase = await createSupabaseServerClient();
+  await supabase.from("todos").update({ completed: completed }).eq("id", id);
+  revalidatePath("/todo");
+}
